2215ab644ce5ca70479b0fde06267124c359c508
lisong <1990691193@qq.com>
2021-11-11 08:12:01 +0000

    1

---
+++ src/layouts/SlideBarLayout/SildeBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <q-card class="q-pa-md">
+      <q-card-section class="row justify-center">
+        <q-avatar
+         size="100px"
+        >
+          <q-img
+            src="https://uploadfile.bizhizu.cn/2016/0812/20160812061159971.jpg"
+            class="fit"
+          />
+        </q-avatar>
+      </q-card-section>
+      <q-card-section class="row justify-center">
+        <div class="text-h5">远方有你伴余生</div>
+        <div class="text-subtitle q-mt-sm">事需缓急，欲速不达也</div>
+      </q-card-section>
+      <q-card-section>
+        <q-btn
+          icon="fab fa-github"
+          type="a"
+          href="https://www.github.io"
+          target="_black"
+          flat
+        ></q-btn>
+      </q-card-section>
+
+    </q-card>
+
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'SlideBar'
+})
+</script>
--- src/layouts/SlideBarLayout/SlideBarLayout.vue
+++ src/layouts/SlideBarLayout/SlideBarLayout.vue
@@ -1,16 +1,16 @@
 <template>
   <q-page padding class="row q-pt-xl justify-evenly">
-    <div class="col-3 bg-grey-7">
-      left
-    </div>
+    <slide-bar class="col-3 bg-grey-7" />
     <div class="col-7 bg-grey-7" >right</div>
   </q-page>
 </template>
 
 <script lang="ts" >
+import SlideBar from './SildeBar.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'SlideBarLayout'
+  name: 'SlideBarLayout',
+  components: { SlideBar }
 })
 </script>
