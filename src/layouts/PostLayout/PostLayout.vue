4c62664b1b3d256c6aae7f1ccd72912c580bb886
lisong <1990691193@qq.com>
2021-11-13 03:29:56 +0000

    11

--- src/layouts/PostLayout/PostLayout.vue
+++ src/layouts/PostLayout/PostLayout.vue
@@ -1,13 +1,44 @@
 <template>
- <post-list />
+  <post-list class="post-list">
+    <q-infinite-scroll
+      @load="onLoad"
+      :offset="250"
+      scroll-target=".post-list"
+    >
+      <post-item v-for="(item, index) in posts" :key="index" ></post-item>
+
+       <template v-slot:loading>
+        <div class="row justify-center q-my-md">
+          <q-spinner-dots color="primary" size="40px" />
+        </div>
+      </template>
+    </q-infinite-scroll>
+  </post-list>
 </template>
 
 <script  lang="ts">
 import PostList from './PostList.vue'
-import { defineComponent } from 'vue'
+import PostItem from './PostItem.vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'PostLayout',
-  components: { PostList }
+  components: { PostList, PostItem },
+  setup () {
+    const posts = ref([{}, {}, {}, {}])
+
+    return {
+      posts,
+      onLoad (index: number, done: () => void) {
+        setTimeout(() => {
+          if(posts.value) {
+            console.log('onload')
+            posts.value.push({}, {}, {}, {})
+            done()
+          }
+        }, 2000)
+      }
+    }
+  }
 })
 </script>
--- src/layouts/PostLayout/PostList.vue
+++ src/layouts/PostLayout/PostList.vue
@@ -1,49 +1,6 @@
 <template>
   <q-list style="width: 100%;">
-    <q-item class="q-px-none" v-for="i of [1,2]" :key="i" >
-      <q-card  style="width: 100%;" >
-        <q-card-section horizontal >
-          <div class="col-4 border-p overflow-hidden">
-            <q-img
-            src="https://cdn.quasar.dev/img/mountains.jpg"
-            class="fit"
-            />
-          </div>
-          <div class="col-8 q-py-md q-px-lg">
-            <div
-              class="text-h5 text-center title-hidden q-mx-auto"
-              style="width: 90%;"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus fugiat odio rem minus aliquam? Explicabo illo, eligendi quas ipsa facilis consectetur vel libero impedit perspiciatis excepturi doloremque ullam iusto enim.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quae maxime accusamus reprehenderit aut pariatur vero possimus corporis adipisci deserunt labore, excepturi perferendis ab impedit consectetur fuga, velit atque minima?
-            </div>
-            <div class="q-mt-sm">
-              <q-chip icon="bookmark" size="xs" >Bookmark</q-chip>
-              <q-chip icon="bookmark" size="xs" >Bookmark</q-chip>
-              <q-chip icon="bookmark" size="xs" >Bookmark</q-chip>
-              <q-chip icon="bookmark" size="xs" >Bookmark</q-chip>
-            </div>
-            <p class="q-mt-xs text-hidden">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem praesentium dolorem omnis ex, atque dicta obcaecati modi sequi optio inventore! Saepe quidem repellendus recusandae incidunt reprehenderit accusamus molestias ea repudiandae.
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic quam minima quod debitis dolore et repellat maiores amet inventore nesciunt placeat dolorem fugit doloribus laudantium, dolores eum. Alias, eos fuga.
-            </p>
-            <q-card-section class="row justify-end q-py-none">
-              <q-btn
-                flat
-                rounded
-                color="primary"
-                label="Click me"
-                type="a"
-                to="/"
-              >
-
-              </q-btn>
-            </q-card-section>
-          </div>
-        </q-card-section>
-      </q-card>
-    </q-item>
-
+      <slot></slot>
   </q-list>
 </template>
 
@@ -54,27 +11,3 @@ export default defineComponent({
   name: 'PostList',
 })
 </script>
-
-<style scoped>
-  .border-p {
-    border-radius: inherit;
-  }
-
-  .text-hidden {
-    display: -webkit-box;
-    -webkit-line-clamp: 4;
-    -webkit-box-orient: vertical;
-    text-overflow:ellipsis;
-    overflow: hidden;
-  }
-
-  .text-indent {
-    text-indent: 2em;
-  }
-
-  .title-hidden {
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-  }
-</style>
