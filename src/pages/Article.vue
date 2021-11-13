5b42a21971fe90e1cfd1fe392f0a2166d6b75b50
lisong <1990691193@qq.com>
2021-11-13 01:26:53 +0000

    11

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -1,12 +1,18 @@
 <template>
-  <div style="height: 250px; overflow: auto">
+  <div class="demo" style="">
     <q-infinite-scroll
       @load="onLoad"
       :offset="250"
+      scroll-target=".demo"
     >
       <div v-for="(item, index) in items" :key="index" class="caption" >
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas atque rerum eaque, reiciendis corporis recusandae eveniet provident doloremque. Vero sit asperiores impedit magnam porro aut nostrum quos aliquid similique deleniti?</p>
       </div>
+      <template v-slot:loading>
+        <div class="row justify-center q-my-md">
+          <q-spinner-dots color="primary" size="40px" />
+        </div>
+      </template>
     </q-infinite-scroll>
 
   </div>
@@ -18,14 +24,15 @@ import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'Article',
   setup () {
-    const items = ref([{}, {}, {}, {}, {}])
+    const items = ref([{}, {}, {}, {}, {}, {}, {}])
     return {
       items,
-      onLoad (index: number, done) {
+      onLoad (index: number, done:() => void) {
         setTimeout(() => {
+          console.log('settimeout')
           if (items.value) {
             console.log('onload')
-            items.value.push({}, {}, {}, {}, {})
+            items.value.push({}, {}, {}, {}, {}, {}, {})
             done()
           }
         }, 2000)
